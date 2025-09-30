9237cdd75f84360e7a21d6f66eb680d7027403b7
Lorenz Bacon <150405033+Renztgm@users.noreply.github.com>
2025-09-30 16:47:35 +0800

    Update test_runner.gd

--- test_runner.gd
+++ test_runner.gd
@@ -1,22 +1,15 @@
 extends SceneTree
 
 func _init():
-	# 🛠️ Skip SQLite/autoload setup if running in CI
-	if OS.has_environment("CI"):
-		print("⚠️ Running in CI mode - skipping SQLite and heavy autoloads")
-
-		# Use root to check/remove autoloads
-		if root.has_node("SaveManager"):
-			root.get_node("SaveManager").queue_free()
-		if root.has_node("AudioManager"):
-			root.get_node("AudioManager").queue_free()
-
-	# Normal GUT setup
 	var gut = load("res://addons/gut/gut.gd").new()
 	root.add_child(gut)
 
+	# Tell GUT which folder(s) to look in
 	gut.set_directories(["res://tests"])
+
+	# Configure output (use constants from gut instance instead of Gut.PRINT_DETAIL)
 	gut.set_print_format(gut.PRINT_DETAIL)
+	# gut.set_verbose(true) # if you also want asserts printed
 
 	# Run tests
 	gut.run_tests()
